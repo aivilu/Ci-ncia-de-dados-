988ee001c90ca76ee2196cc8d4a6a6ca5b67828a
aivilu <163533315+aivilu@users.noreply.github.com>
2024-09-20 11:12:30 -0300

    Add files via upload

---
+++ InformaçõesGlobais.js
@@ -0,0 +1,13 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = document.createElement('p')
+  paragrafo.classList.add('graficos-container__texto')
+  paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente `
+}
+
+vizualizarInformacoesGlobais()
+
+
